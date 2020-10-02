461a4d229b899c217e07610dbe85fe1653dda09a
钱立峰 <805042956@qq.com>
2020-10-02 23:27:11 +0800

    fix

---
+++ precache-manifest.0b0b56ad7a6f4f1654ef65c234f65cf7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3d4abef4c283add45e77b9b177b84a9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2e833e5c12f64d93ea8c",
+    "url": "./static/css/2.abbeff8f.chunk.css"
+  },
+  {
+    "revision": "692adc770aec9c24916e",
+    "url": "./static/css/main.db059658.chunk.css"
+  },
+  {
+    "revision": "2e833e5c12f64d93ea8c",
+    "url": "./static/js/2.42026064.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.42026064.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "692adc770aec9c24916e",
+    "url": "./static/js/main.02ba59e3.chunk.js"
+  },
+  {
+    "revision": "327a06e6357f5cd919f2",
+    "url": "./static/js/runtime-main.9e38079d.js"
+  },
+  {
+    "revision": "07851b01515d3292f6fae2e6e6ff1baf",
+    "url": "./static/media/404.07851b01.jpg"
+  },
+  {
+    "revision": "5f29fd7b5df1f7bf3781459f2d18b8e4",
+    "url": "./static/media/body.5f29fd7b.png"
+  },
+  {
+    "revision": "18ba5e19db0afa1dcf84845911ebfe3a",
+    "url": "./static/media/header.18ba5e19.png"
+  },
+  {
+    "revision": "1f35fac53e93119810867a89a7bb55a8",
+    "url": "./static/media/title.1f35fac5.png"
+  }
+]);
